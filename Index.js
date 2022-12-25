568a7475158fce312407829668d75827a14b860b
WolfWhale <davideon05@gmail.com>
2022-12-24 21:49:56 -0500

    First commit

---
+++ Index.js
@@ -0,0 +1,5 @@
+
+function Hello(firstzName, height, country){
+    alert( "Hello my name is " + firstzName + ", " + "I'm" + height + " and the country i live in is the" + country + ".")
+}
+Hello("David ",  ` 5'7"`, " USA");
